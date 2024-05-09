6eec072f111877c391b81cd5b5bb773291a19c8a
IketheGreat <100230690+IketheGreat@users.noreply.github.com>
2024-05-09 16:12:19 +0000

    push latest code to github (#1)
    * push code to github
    
    * add space at the end of files
    
    ---------
    
    Co-authored-by: isaac <isaac.briandt@kofa.co>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./app/pages/Home";
+import Cart from "./app/pages/Cart";
+import { UseAppSelector, useAppDispatch } from "./app/hooks";
+import { useEffect } from "react";
+import { cartTotal } from "../features/shopSlice";
 
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
+  const { cartItems } = UseAppSelector((state) => state.shop);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(cartTotal());
+  }, [cartItems, dispatch]);
 
   return (
-    <div>The start of my project</div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        theme="colored"
+        progressStyle={undefined}
+      />
+    </Provider>
+  </React.StrictMode>
+);
